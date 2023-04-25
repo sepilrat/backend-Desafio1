6adfcc2680b11e6663f34c91967272080818d8fa
rossi.juan <rossi.juan.1980@gmail.com>
2023-04-24 21:51:51 -0300

    primer commit

--- flat.js
+++
@@ -1,7 +0,0 @@
-const notas =[[10,5,2],[1,2,4], 6,7]
-const aplanado =notas.flat();
-console.log (aplanado);
-
-const total = aplanado.reduce((parcial,nota) => parcial + nota, 0);
-const promedio = total / aplanado.length;
-console.log (promedio);
